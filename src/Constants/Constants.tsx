27996c4610e0ad16c19d744bea6291db0a6f54e4
Lalit-Jethani <lalit.jethani@gmail.com>
2018-03-18 12:18:54 +0530

    env variables

--- src/Constants/Constants.tsx
+++ src/Constants/Constants.tsx
@@ -1,9 +1,11 @@
+ 
 
 export const TRIP_DETAILS = 'TRIP_DETAILS';
 export const REQUEST_DEALS = "REQUEST_DEALS";
 export const RECIEVE_DEALS = "RECIEVE_DEALS";
 export type TRIP_DETAILS_TYPE = typeof TRIP_DETAILS;
-export const API_URL = process.env.NODE_ENV === 'production' ? 
 
- "https://tripfinder-lalit.herokuapp.com": "http://localhost:3000" ;
+export const REACT_APP_API_URL = process.env.NODE_ENV === 'production' ? 
+
+"https://tripfinder-lalit.herokuapp.com": "http://localhost:3000" ;
 
