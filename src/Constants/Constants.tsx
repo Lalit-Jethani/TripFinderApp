5e7015f041bb4f80e80fb18e3d527aa0027d14a2
Lalit-Jethani <lalit.jethani@gmail.com>
2018-03-18 11:57:28 +0530

    bug fix

--- src/Constants/Constants.tsx
+++ src/Constants/Constants.tsx
@@ -3,7 +3,7 @@ export const TRIP_DETAILS = 'TRIP_DETAILS';
 export const REQUEST_DEALS = "REQUEST_DEALS";
 export const RECIEVE_DEALS = "RECIEVE_DEALS";
 export type TRIP_DETAILS_TYPE = typeof TRIP_DETAILS;
-export const API_URL = process.env.NODE_ENV === 'development' ? 
+export const API_URL = process.env.NODE_ENV === 'production' ? 
 
- "http://localhost:3000" : "https://tripfinder-lalit.herokuapp.com" ;
+ "https://tripfinder-lalit.herokuapp.com": "http://localhost:3000" ;
 
